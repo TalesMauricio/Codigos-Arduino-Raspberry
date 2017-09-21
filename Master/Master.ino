bcd38756a1e18662c9fa902968e726fc41dca7cd
TalesMauricio <talesmauricioraulino@gmail.com>
2017-09-21 16:07:07 -0300

    teste
    teste

--- Master/Master.ino
+++ Master/Master.ino
@@ -1,14 +1,9 @@
  
  
  /** RF24Mesh_Example_Master.ino by TMRh20
-  * 
+  * simples
   *
-  * This example sketch shows how to manually configure a node via RF24Mesh as a master node, which
-  * will receive all data from sensor nodes.
   *
-  * The nodes can change physical or logical position in the network, and reconnect through different
-  * routing nodes as required. The master node manages the address assignments for the individual nodes
-  * in a manner similar to DHCP.
   *
   */
   
@@ -28,12 +23,9 @@ RF24Mesh mesh(radio,network);
 uint32_t displayTimer = 0;
 
 void setup() {
-  Serial.begin(115200);
-
-  // Set the nodeID to 0 for the master node
+  Serial.begin(115200); 
   mesh.setNodeID(0);
   Serial.println(mesh.getNodeID());
-  // Connect to the mesh
   mesh.begin();
 
 }
@@ -41,15 +33,11 @@ void setup() {
 
 void loop() {    
 
-  // Call mesh.update to keep the network updated
-  mesh.update();
   
-  // In addition, keep the 'DHCP service' running on the master node so addresses will
-  // be assigned to the sensor nodes
-  mesh.DHCP();
+  mesh.update();  
+  mesh.DHCP();  
   
-  
-  // Check for incoming data from the sensors
+ 
   if(network.available()){
     RF24NetworkHeader header;
     network.peek(header);
