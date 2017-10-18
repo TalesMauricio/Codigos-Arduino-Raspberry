9edfae420e0350212e7a176eb1633fe00837def9
Daniel Borges <danborges06@hotmail.com.br>
2017-10-18 05:51:52 -0200

    Transmissão por I2C implementada

--- Modulos/placa_slave/placa_slave.ino
+++ Modulos/placa_slave/placa_slave.ino
@@ -1,22 +1,11 @@
 #include <Wire.h>
-#include "pacote.h"
+#include "dados.h"
 
-#define nodeID 5
-#define horas 2
-#define minutos 40
-#define nivel 80
-
-union stream
-{
-    pacote_t pacote;
-    char bytes[ sizeof(pacote_t) ];
-};
-
-stream pacote = {nodeID, horas, minutos, nivel, 50, 0};
+Dados_t dados = {18, 1, 100, 60, 500};
 
 void setup() {
-  
-  Wire.begin(8);                // join i2c bus with address #8
+
+  Wire.begin(0);                // join i2c bus with address #8
   Wire.onRequest(requestEvent); // register event
 }
 
@@ -27,6 +16,10 @@ void loop() {
 // function that executes whenever data is requested by master
 // this function is registered as an event, see setup()
 void requestEvent() {
-  Wire.write(pacote.bytes); // respond with message of 6 bytes
-  // as expected by master
+  Wire.write(dados.bytes);
+
+  dados.valor.temperatura++;
+  dados.valor.ph++;
+  dados.valor.turbidez++;
+  dados.valor.condutividade++;
 }
