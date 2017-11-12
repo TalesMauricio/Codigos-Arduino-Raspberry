0069297db4dea2a9d0b5d92f0bc920da55cbc6a5
Daniel Borges <danborges06@hotmail.com.br>
2017-11-12 12:46:50 -0200

    Arquivos reorganizados

--- Alimentadores-b/controle.ino
+++
@@ -1,9 +0,0 @@
-//Aqui vai tudo relacionado ao acionamento de cargas, medição e controle dos motores
-
-void alimentaPeixes(float valorAlimento)
-{
-  digitalWrite(fuso, HIGH);
-  while(lerCelula()<=valorAlimento) delay(100);
-  digitalWrite(fuso, LOW);
-}
-
--- Alimentadores-b/sinalizacao.ino
+++
@@ -1,1 +0,0 @@
-//Aqui vai tudo relacionado a tela LCD/LEDs que venham a existir
