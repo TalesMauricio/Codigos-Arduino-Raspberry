dd18ebccc57d247088993fef6b67d8996f1fa66b
Daniel Borges <danborges06@hotmail.com.br>
2017-11-12 05:51:57 -0200

    Código reorganizado

--- Alimentadores/nivel.ino
+++ Alimentadores/nivel.ino
@@ -1,26 +1,16 @@
 //Aqui vai tudo relacionado a medição de nível de profundidade
 
-#define profund 100  // profundidade da caixa (aqui vc coloca a pronfudidade da caixa em CM)
-
 void Nivel()
 { 
-  /* Rotina de funionamento para o Sensor Ultrasson 1 */  
   long medianivel = 0;
   for (int i=1; i <= 10; i++){
-    digitalWrite(trigPin, LOW); // seta o pino 6 com um pulso baixo "LOW"
-    delayMicroseconds(2); // delay de 2 microssegundos
-    digitalWrite(trigPin, HIGH); // seta o pino 6 com pulso de "HIGH"
-    delayMicroseconds(10); // delay de 10 microssegundos
-    digitalWrite(trigPin, LOW); // seta o pino 12 com pulso baixo novamente
-    long tempo = pulseIn(echoPin, HIGH); // pulseIn lê o tempo entre a chamada e o pino entrar em high
+    long duração = medirTempoEco()
     
     //////////@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@    melhorar o filtro @@@@@@@@@@@@@@@@@@@@@@@@@@@@//////////
-    
     long nivel_atual = tempo / 58 *100 / profund ; //Esse calculo é baseado em s = v . t, lembrando que o tempo vem dobrado ///// distancia = tempo / (29 * 2)//// ..........................
      
-      if(nivel_atual <= 100){
+      if(nivel_atual <= profund)
         medianivel = (medianivel + nivel_atual);
-      }
       else{
         i--;
         delay(100);
@@ -29,5 +19,14 @@ void Nivel()
   }
   medianivel = 100-(medianivel/10);
   nivelRacao = medianivel;
+}
+
+long medirTempoEco()
+{    /* Rotina de funionamento para o Sensor Ultrasson 1 */  
+    digitalWrite(trigPin, HIGH); // seta o pino 6 com pulso de "HIGH"
+    delayMicroseconds(10); // delay de 10 microssegundos
+    digitalWrite(trigPin, LOW); // seta o pino 6 com pulso baixo novamente
+
+    return pulseIn(echoPin, HIGH); // pulseIn lê o tempo entre a chamada e o pino entrar em high
 }
 
