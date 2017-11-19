0fd76e67a5d57b78d8307c52afed22790dc4acf6
Daniel Borges <danborges06@hotmail.com.br>
2017-11-19 02:28:52 -0200

    Algumas mudanças simples

--- Projeto_crema_controle/Master.cpp
+++ Projeto_crema_controle/Master.cpp
@@ -1,12 +1,12 @@
- 
- 
+
+
  /*	Master com controle
-  * 
+  *
   * Com controle de estados
   *
   */
-  
-#include "RF24Mesh/RF24Mesh.h"  
+
+#include "RF24Mesh/RF24Mesh.h"
 #include <RF24/RF24.h>
 #include <RF24Network/RF24Network.h>
 #include <stdio.h>
@@ -15,7 +15,7 @@
 #include <stdlib.h>
 
 
-RF24 radio(RPI_V2_GPIO_P1_15, BCM2835_SPI_CS0, BCM2835_SPI_SPEED_8MHZ);  
+RF24 radio(RPI_V2_GPIO_P1_15, BCM2835_SPI_CS0, BCM2835_SPI_SPEED_8MHZ);
 RF24Network network(radio);
 RF24Mesh mesh(radio,network);
 uint32_t displayTimer=0;
@@ -27,12 +27,12 @@ erro:
     2: ||      10  ||  Tempo de alimentação muito grande
     4: ||     100  ||  Porta do alimntador com defeito
     8: ||    1000  ||  Tampa do reservatório aberta
-   16: ||   10000  ||  
+   16: ||   10000  ||
 */
 
 struct pacote_t
 {
-  uint8_t alimentadorID; // ID de qual alimentador a informação está sendo enviada  
+  uint8_t alimentadorID; // ID de qual alimentador a informação está sendo enviada
   uint8_t hora;          // Hora que foi enviada
   uint8_t minuto;        // Minuto que foi enviada
   uint8_t nivel;         // Nível de ração no reservatório 1
@@ -52,28 +52,27 @@ struct diretriz_t
   unsigned short inicio_hora;       // hora de início
   unsigned short inicio_minuto;     // hora de início
 //  unsigned short frequencia;        // frequencia de alimentação
-  unsigned short qtd;               // quantidade de raçao despejada          
+  unsigned short qtd;               // quantidade de raçao despejada
 };
 
 struct relogio_t
 {
-  unsigned char segu;   // ajustar o segundo do RTC
-  unsigned char minu;   // ajustar o minuto do RTC 
-  unsigned char hora;   // ajustar a hora do RTC
-  unsigned char dias;   // ajustar o dia da semana do RTC
-  unsigned char diam;   // ajustar o dia do mes do RTC
-  unsigned char mess;   // ajustar o mes do RTC
-  unsigned char anoo;   // ajustar o ano do RTC
+  uint8_t segu;   // ajustar o segundo do RTC
+  uint8_t minu;   // ajustar o minuto do RTC
+  uint8_t hora;   // ajustar a hora do RTC
+  uint8_t diam;   // ajustar o dia do mes do RTC
+  uint8_t mess;   // ajustar o mes do RTC
+  uint8_t anoo;   // ajustar o ano do RTC
 };
 
-	unsigned char segui;
-	unsigned char minui;
-	unsigned char horai;	
-	unsigned char diasi;
-	unsigned char diami;
-	unsigned char messi;	
-	unsigned char anooi;
-	unsigned char i=0;
+	uint8_t segui;
+	uint8_t minui;
+	uint8_t horai;
+	uint8_t diasi;
+	uint8_t diami;
+	uint8_t messi;
+	uint8_t anooi;
+	uint8_t i=0;
 
 
 // */
@@ -90,55 +89,55 @@ pacote_t pacote;
 relogio_t relogio;
 //------------------------------------------------------
 
-int main(int argc, char** argv) {  
- 
+int main(int argc, char** argv) {
+
 	mesh.setNodeID(0);
 	printf("start\n");
 	mesh.begin();
 	radio.setPALevel(RF24_PA_MAX);
 	radio.setDataRate(RF24_1MBPS);
 	radio.setCRCLength(RF24_CRC_16);
-  
+
 	radio.printDetails();
-	
-	
+
+
 	// char url[]="nodes.txt";
 	//FILE *arq, *arq_medidas;
-  
 
-	while(1)	
+
+	while(1)
 	{
 		arq = fopen("nodes.txt", "r");
 		if(arq == NULL)
-		{ // se nao foi possivel abrir o arquivo...			
-			Rx(); // inicia a leitura ###################### se possivel fazer um loop por um determinado tempo ###########								
+		{ // se nao foi possivel abrir o arquivo...
+			Rx(); // inicia a leitura ###################### se possivel fazer um loop por um determinado tempo ###########
 		}
 		else
 		{
-			
-//			while( (fscanf(arq,"%hu %hu %hu %hu %hu\n", 
+
+//			while( (fscanf(arq,"%hu %hu %hu %hu %hu\n",
 			while( (fscanf(arq,"%hu %hu %hu %hu\n",
-							   &diretriz.alimentID, 
-							   &diretriz.inicio_hora, 
-							   &diretriz.inicio_minuto, 
-//							   &diretriz.frequencia, 
+							   &diretriz.alimentID,
+							   &diretriz.inicio_hora,
+							   &diretriz.inicio_minuto,
+//							   &diretriz.frequencia,
 							   &diretriz.qtd))!=EOF )
 			{
-				Tx();				
+				Tx();
 				//printf(" ||||   %hu %hu %hu %hu %hu  ", diretriz.alimentID, diretriz.inicio_hora, diretriz.inicio_minuto, diretriz.frequencia, diretriz.qtd);
-			}	
-					
+			}
+
 			fclose(arq);
 //		system("./apaga_arquivo");
-			
+
 		}
-	
+
 		if(millis() - displayTimer >= 100000){
 			displayTimer = millis();
 //			Tx_hora_correta();
-	
+
 		}
-		
+
 	}
 return 0;
 }
@@ -147,20 +146,20 @@ return 0;
 void Rx()
 {
 	mesh.update();
-	mesh.DHCP();	
-	
-    
+	mesh.DHCP();
+
+
 	if(network.available())
 	{    // retirar o while
 		RF24NetworkHeader header;
-		network.peek(header);    
-		
+		network.peek(header);
+
 		network.read(header,&pacote,sizeof(pacote));
-		
+
 		switch(header.type)
 		{
 			case 'M':
-			
+
 				Obter_comparador();
 				if(pacote.hora != horai || pacote.minuto != minui)
 				{
@@ -168,37 +167,37 @@ void Rx()
 					printf("hora do alimentador %hu esta errada \n", pacote.alimentadorID);
 					Tx_hora_correta();
 				}
-				
-				
+
+
 				printf("Rx:");
 				printf("  1-NodeID: %hu  2-Hora: %hu  3-Minuto: %hu  4-Nivel: %hu  5-Bateria: %hu 6-Erro: %hu   temp %hu ph %hu tur %hu  cond %hu oxi %hu\n",
-				        pacote.alimentadorID, 
-				        pacote.hora, 
-				        pacote.minuto, 
-				        pacote.nivel, 
-				        pacote.bateria, 
+				        pacote.alimentadorID,
+				        pacote.hora,
+				        pacote.minuto,
+				        pacote.nivel,
+				        pacote.bateria,
 				        pacote.erro,
 				        pacote.temperatura,
 				        pacote.ph,
 				        pacote.turbidez,
 				        pacote.condutividade,
-				        pacote.oxigen); 
-			
-//			char url [] = " medidas_tanques.txt " ;			
-			
+				        pacote.oxigen);
+
+//			char url [] = " medidas_tanques.txt " ;
+
 				arq_medidas = fopen ("medidas_tanques.txt", "a" );
 				if (arq_medidas == NULL )
 				{
 					printf ("Erro, nao foi possivel abrir o arquivo \n");
 				}
-				else 
+				else
 				{
-					fprintf(arq_medidas, "%hu %hu %hu %hu %hu %hu %hu %hu %hu %hu %hu\n" , 
-										  pacote.alimentadorID, 
-										  pacote.hora, 
-                                          pacote.minuto, 
-                                          pacote.nivel, 
-                                          pacote.bateria, 
+					fprintf(arq_medidas, "%hu %hu %hu %hu %hu %hu %hu %hu %hu %hu %hu\n" ,
+										  pacote.alimentadorID,
+										  pacote.hora,
+                                          pacote.minuto,
+                                          pacote.nivel,
+                                          pacote.bateria,
                                           pacote.erro,
                                           pacote.temperatura,
 										  pacote.ph,
@@ -206,14 +205,14 @@ void Rx()
 										  pacote.condutividade,
 										  pacote.oxigen);
 				}
-			fclose (arq_medidas);		
-			
-			break;        
-		
-		default:  network.read(header,0,0); 
-			printf("Rcv bad type %d from 0%o\n",header.type,header.from_node); 
+			fclose (arq_medidas);
+
 			break;
-		}    
+
+		default:  network.read(header,0,0);
+			printf("Rcv bad type %d from 0%o\n",header.type,header.from_node);
+			break;
+		}
 	}
 }
 
@@ -222,29 +221,29 @@ void Rx()
 void Tx()
 {
 	mesh.update();
-		
-	if (!mesh.write(&diretriz, 'D', sizeof(diretriz), diretriz.alimentID)) 
+
+	if (!mesh.write(&diretriz, 'D', sizeof(diretriz), diretriz.alimentID))
 	{
 		printf("Nao enviado para %hu \n", diretriz.alimentID);
-		delay(2000);	
-	
-      
-	} 
-	else 
-	{  
-//		printf("Tx: ID: %hu  inicH: %hu inicM: %hu  freq: %hu  qtd: %hu \n", 
-		printf("Tx: ID: %hu  inicH: %hu inicM: %hu  qtd: %hu \n", 
-		       diretriz.alimentID, 
-		       diretriz.inicio_hora, 
-		       diretriz.inicio_minuto, 
-//		       diretriz.frequencia, 
-		       diretriz.qtd); // somente para testes 
-		       	        
+		delay(2000);
+
+
+	}
+	else
+	{
+//		printf("Tx: ID: %hu  inicH: %hu inicM: %hu  freq: %hu  qtd: %hu \n",
+		printf("Tx: ID: %hu  inicH: %hu inicM: %hu  qtd: %hu \n",
+		       diretriz.alimentID,
+		       diretriz.inicio_hora,
+		       diretriz.inicio_minuto,
+//		       diretriz.frequencia,
+		       diretriz.qtd); // somente para testes
+
 		delay(2000);
 //- fazer comando para mover arquivo para outra pasta....
 //system("rm nodes.txt");
-	} 	
-	
+	}
+
 }
 //////////////////////////////////////////////// Tx_hora_correta
 void Tx_hora_correta()
@@ -253,10 +252,10 @@ void Tx_hora_correta()
 	struct tm *time_info;
 	char segu1[3];
 	char minu1[3];
-	char hora1[3];	
+	char hora1[3];
 	char dias1[2];
 	char diam1[3];
-	char mess1[3];	
+	char mess1[3];
 	char anoo1[5];
 
 	current_time = time(NULL);
@@ -268,49 +267,49 @@ void Tx_hora_correta()
 	strftime(diam1, 3, "%d", time_info);
 	strftime(mess1, 3, "%m", time_info);
 	strftime(anoo1, 5, "%g", time_info);
-	
+
 	segui=atoi(segu1);
 	minui=atoi(minu1);
-	horai=atoi(hora1);	
+	horai=atoi(hora1);
 	diasi=atoi(dias1);
 	diami=atoi(diam1);
-	messi=atoi(mess1);	
+	messi=atoi(mess1);
 	anooi=atoi(anoo1);
-	
+
 	mesh.update();
-	relogio = {segui, minui, horai, diasi, diami, messi, anooi};	
-	if (!mesh.write(&relogio, 'T', sizeof(relogio), pacote.alimentadorID)) 
+	relogio = {segui, minui, horai, diasi, diami, messi, anooi};
+	if (!mesh.write(&relogio, 'T', sizeof(relogio), pacote.alimentadorID))
 	{
 		printf("Nao foi possivel atualizar o relogio do alimentador %hu \n", pacote.alimentadorID);
 		printf("####################################################\n\n");
-		delay(2000);      
-	} 
-	else 
-	{  
-		
+		delay(2000);
+	}
+	else
+	{
+
 		printf("Tx-hora-correta:\n");
 		printf("alimentador id: %hu recebe: %i : %i : %i    %i / %i / %i \n", pacote.alimentadorID, horai, minui, segui, diami, messi, anooi);
-		printf("####################################################\n\n"); 		       	        
-		delay(2000);	
-	} 	
-	
+		printf("####################################################\n\n");
+		delay(2000);
+	}
+
 }
 
 
 //////////////////////////////////////////////// Obter_comparador
 void Obter_comparador()
-{ 
+{
 	time_t current_time;
 	struct tm *time_info;
 	char minu[3];
 	char hora[3];
-		
+
 	current_time = time(NULL);
 	time_info = localtime(&current_time);
 	strftime(minu, 3, "%M", time_info);
 	strftime(hora, 3, "%H", time_info);
 	minui=atoi(minu);
 	horai=atoi(hora);
-		
+
 }
-      
+
