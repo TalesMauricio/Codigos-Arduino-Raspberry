c195f8bb92c5674f021e98e43b55b1e2aa692df4
TalesMauricio <talesmauricioraulino@gmail.com>
2017-11-19 22:48:23 -0200

    add melhoramento

---
+++ teste2 tales/Alimentadores/Alimentadores.ino
@@ -0,0 +1,37 @@
+
+/* 
+ *   Projeto Crema
+ *   Alimentador de peixes;
+ *  
+ *   Este código é o principal. Aqui todo o algorítmo deve ser descrito por funções de alto grau de abstração. Isto é necessário para manter o
+ *   código o mais compacto o possível e possibilitar uma melhor compreensão do código como todo, sem se perder em detalhes menores. A ideia é
+ *   que da forma que for implementado, o código explique por si só, em procedimentos básicos, o algorítmo a ser desempenhado.
+ *   
+ *   Todos os pinos e variaveis globais estão em defines.h
+ *   
+ *   Todas as structs estão em estruturas.cpp
+*/
+  // Só devem ser declaradas as bibliotecas de funções locais de alta abstração. Nada de bibliotecas do arduino ou implementações de baixo nível
+#include "defines.h"
+
+diretriz_t diretriz = {61,61,61,61,61,61,61,61,61,61,61,61,61,61,61,61,61,61,61};
+
+void setup() {
+  Serial.begin(115200);
+  configPins();  //Declarado em controle
+  initComunic(); //Declarado em comunicacao
+}
+
+void loop() {
+  atualizarMalha();  //Declarado em comunicacao
+  if(conectado)
+  {
+    recebeDiretriz();
+    enviaPacote();
+  }
+  delay(1000);
+  
+  ini_prot_aliment();
+//  medirBateria();
+  reqMedidasSec();
+}
---
+++ teste2 tales/Alimentadores/controle.ino
@@ -0,0 +1,109 @@
+//Aqui vai tudo relacionado ao acionamento de cargas, medição e controle dos motores
+
+#include <Servo.h>
+
+Servo porta;
+
+void configPins() {
+  Wire.begin(); //I2C
+  
+  // nivel de ração com HC-SR04
+  pinMode(echoPin, INPUT); // define o pino 7 como entrada (recebe)
+  pinMode(trigPin, OUTPUT); // define o pino 6 como saida (envia)
+  
+  //Célula de carga
+  pinMode(ADDO, INPUT_PULLUP);   
+  pinMode(ADSK, OUTPUT);
+  
+  //motor(fuso) e servo(porta) ****/
+  pinMode(fuso, OUTPUT);
+  porta.attach(servo);
+  
+  //medição de bateria
+  pinMode(leBatPin, INPUT);   
+  pinMode(enBatPin, OUTPUT);
+  }
+
+
+
+void ini_prot_aliment()
+{
+//  Serial.print(diretriz.minutod1);
+//  Serial.print("   ");  
+//  Serial.print(minute());
+//  Serial.print("   ");
+//  alimentaPeixes(diretriz.qtdd1);
+
+ for(int cont=1; cont<=6; cont++)
+  {
+ if(diretriz.horad[cont] == hour() && diretriz.minutod[cont] == minute() && second() == 30) {
+    Serial.println(" "); 
+    Serial.print("chegou a hora de alimentar");
+    Serial.print(cont);
+    Serial.print(": "); 
+    Serial.print(diretriz.horad[cont]);  
+    Serial.print(":");  
+    Serial.print(diretriz.minutod[cont]);
+    Serial.print("  com : ");
+    Serial.print(diretriz.qtdd[cont]);
+    Serial.println("kg");  
+ }
+  }
+
+  
+}
+
+
+
+//void alimentaPeixes(float valorAlimento)
+void alimentaPeixes(unsigned long valorAlimento)
+{
+  //AlarmId id = Alarm.getTriggeredAlarmId();
+  //float valorAlimento, massa = qtdRacao[id];
+
+
+  /*
+  float valorAlimento;
+  
+  bool pronto = false;
+  
+  while(!pronto)
+  {
+    if(massa > 10.0)
+      valorAlimento = 10.0;
+    else {
+      valorAlimento = massa;
+      pronto = true;
+    }
+    DespejarRacao(valorAlimento);
+    esvaziarCompatimento();
+  }
+  */
+  valorAlimento = 7677514; //valor exemplo
+  digitalWrite(fuso, HIGH);
+  while(lerCelula()>=valorAlimento) delay(100);
+  digitalWrite(fuso, LOW);
+  
+  esvaziarCompatimento();
+  delay(60000);
+}
+
+/*
+void DespejarRacao(float pesoAlimento)
+{
+  float bufferPeso[20] = {0};
+  
+  digitalWrite(fuso, HIGH);
+  while( obterPeso(bufferPeso) <= pesoAlimento ); //delay(100);
+  
+  digitalWrite(ADSK, HIGH);
+  digitalWrite(fuso, LOW);
+}*/
+
+void esvaziarCompatimento()
+{
+  porta.write(anguloPortaAbert);
+  delay(tempoPortaAbert);
+  porta.write(anguloPortaFecha);
+}
+
